ad3b907fa7a96e8d6558282a433445d46b0772c9
Maxim Gnatenko <mgnatenko@gmail.com>
2017-05-21 17:06:46 +0700

    setup backend testing environment

---
+++ backend/tests/EnvironmentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Acme\Pay;
+
+class EnvironmentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPhpUnitIsSetUp()
+    {
+        $this->assertTrue(true);
+    }
+}
