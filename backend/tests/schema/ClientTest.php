a5519cc20d6917b360c50d4379c2c1fbb84c2558
Maxim Gnatenko <mgnatenko@gmail.com>
2017-05-27 09:29:44 +0700

    implement tests for client data type

---
+++ backend/tests/schema/ClientTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Acme\Pay\Schema;
+
+use Acme\Pay\Test\SchemaTestCase;
+
+class ClientTest extends SchemaTestCase
+{
+    protected static $schemaId = 'http://acmepay.local/schema/client.json#';
+
+    public function testRegularClientIsValid()
+    {
+        $client = json_decode(<<<JSON
+{
+    "name": "Jon Doe",
+    "country": "Angola",
+    "city": "Luanda"
+}
+JSON
+        );
+
+        self::assertValid($client);
+    }
+
+    public function testExtraParamsAreNotAllowed()
+    {
+        $client = json_decode(<<<JSON
+{
+    "name": "Jon Doe",
+    "country": "Angola",
+    "city": "Luanda",
+    "job": "Plumber"
+}
+JSON
+        );
+
+        self::assertNotValid($client);
+    }
+
+    public function testOnlyNameIsMandatory()
+    {
+        $client = json_decode(<<<JSON
+{
+    "name": "Jon Doe"
+}
+JSON
+        );
+
+        self::assertValid($client);
+    }
+
+}
